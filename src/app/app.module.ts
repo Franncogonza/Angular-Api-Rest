17266c0cfcd48c0d06cca8d5719bf7689d3dc824
Franco Gonzalez <fgonzalez@intranet.fyo.com>
2020-09-08 18:52:55 -0300

    servicio, métodos, llamada a api y creacion componente user-details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{ HttpClientModule } from'@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponent } from './components/home/home.component';
+import { UserDetailComponent } from './components/user/user-detail.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +15,13 @@ import { HomeComponent } from './components/home/home.component';
     NavbarComponent,
     AboutComponent,
     ContactComponent,
-    HomeComponent
+    HomeComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { ReqresService } from "../../services/reqres.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  users: any[] = [];
+  constructor(private reqresService: ReqresService) {}
 
   ngOnInit(): void {
+    this.setView();
+  }
+
+  setView() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.reqresService.getUsers().subscribe(
+      (res: any) => {
+        this.users = res.data;
+        console.log(this.users);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  userDetails(id: number) {
+    console.log("Id usuario: ", id);
   }
 
 }
